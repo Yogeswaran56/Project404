052766eb8cc2d5a257b5a6954151586e5978010d
Sathvik Rijo <sathvikrijo@gmail.com>
2019-01-09 19:17:46 +0530

    vendor side welcome screen activity

---
+++ app/src/main/java/com/example/yogesh/project/Welcome_screen.java
@@ -0,0 +1,13 @@
+package com.example.yogesh.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
