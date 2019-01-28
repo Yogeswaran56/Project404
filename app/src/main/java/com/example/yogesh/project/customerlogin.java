3c692de517745ce081feb7e1e3ec009d25a29f38
viola-grace <violagrace22@gmail.com>
2019-01-28 17:51:33 +0530

    Merge branch 'master' of C:\Users\Viola\AndroidStudioProjects\Project404 with conflicts.

---
+++ app/src/main/java/com/example/yogesh/project/customerlogin.java
@@ -0,0 +1,65 @@
+package com.example.yogesh.project;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class customerlogin extends AppCompatActivity {
+
+    private Button button;
+     private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn_login);
+
+        getSupportActionBar().hide();
+
+        // init constraintLayout
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+
+        // initializing animation drawable by getting background from constraint layout
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+
+        // setting enter fade animation duration to 5 seconds
+        animationDrawable.setEnterFadeDuration(500);
+
+        // setting exit fade animation duration to 2 seconds
+        animationDrawable.setExitFadeDuration(500);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view){
+
+                Intent i = new Intent(customerlogin.this, customerhomepage.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
+    }
+}
