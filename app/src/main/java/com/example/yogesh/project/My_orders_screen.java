dc7a6029ebd48a1720f192d83099706a5423cc8c
Yogeswaran S <yogi.waran56@gmail.com>
2019-01-28 20:56:47 +0530

    Yogi commit

---
+++ app/src/main/java/com/example/yogesh/project/My_orders_screen.java
@@ -0,0 +1,13 @@
+package com.example.yogesh.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class My_orders_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_orders_screen);
+    }
+}
