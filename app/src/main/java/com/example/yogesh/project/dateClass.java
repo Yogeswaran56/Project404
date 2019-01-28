74cd607a54e7e7cc42ce26c7b7cc76e325a6cf02
Sathvik Rijo <sathvikrijo@gmail.com>
2019-01-28 19:20:05 +0530

    classes for schedule information

---
+++ app/src/main/java/com/example/yogesh/project/dateClass.java
@@ -0,0 +1,23 @@
+package com.example.yogesh.project;
+
+public class dateClass {
+    private int day, month, year;
+
+    public dateClass(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
