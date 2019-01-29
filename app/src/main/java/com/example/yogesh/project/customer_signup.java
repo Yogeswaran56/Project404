91b22da252cd7550d848cfa2c65c73afe5d71edb
Yogeswaran56 <yogi.waran56@gmail.com>
2019-01-29 05:36:19 -0800

    auth for both

--- app/src/main/java/com/example/yogesh/project/customer_signup.java
+++ app/src/main/java/com/example/yogesh/project/customer_signup.java
@@ -1,11 +1,14 @@
 package com.example.yogesh.project;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -39,9 +42,26 @@ public class customer_signup extends AppCompatActivity implements View.OnClickLi
             email = editText_email.getText().toString();
             password = editText_password.getText().toString();
 
+            if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "E-mail ID and Password cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "E-mail ID cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
             firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), customerhomepage.class));
+                    }
                 }
             });
         }
