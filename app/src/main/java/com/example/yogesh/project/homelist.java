459616aa637c13ae11b4b747cef7ba0daa84c4e3
viola-grace <violagrace22@gmail.com>
2019-01-30 14:56:26 +0530

    push

--- app/src/main/java/com/example/yogesh/project/homelist.java
+++ app/src/main/java/com/example/yogesh/project/homelist.java
@@ -13,25 +13,44 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class homelist extends Fragment implements View.OnClickListener {
+public class homelist extends Fragment{
 
-    private CardView vegcardview,fruitcardview ;
+//    private CardView vegcardview,fruitcardview ;
+//
+//    public homelist() {
+//        // Required empty public constructor
+//    }
+//
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.activity_customerhomepage, container, false);
+//
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//
+//    }
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_homelist,
+            container, false);
+    CardView cardview = (CardView) rootView.findViewById(R.id.vegecard);
+    cardview.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            updateDetail();
+        }
+    });
+    return rootView;
+}
 
-    public homelist() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_customerhomepage, container, false);
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
+    public void updateDetail() {
+        Intent intent = new Intent(getActivity(), vegetables.class);
+        startActivity(intent);
     }
 }
