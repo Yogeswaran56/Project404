4fd819a64704b48c15fd62b5fefe8c08bed82e8d
viola-grace <violagrace22@gmail.com>
2019-01-31 10:57:10 +0530

    homelist

--- app/src/main/java/com/example/yogesh/project/homelist.java
+++ app/src/main/java/com/example/yogesh/project/homelist.java
@@ -1,13 +1,26 @@
 package com.example.yogesh.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class homelist extends AppCompatActivity {
-
+public class homelist extends AppCompatActivity implements View.OnClickListener{
+    private CardView veg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_homelist);
+        setContentView(R.layout.activity_homelist);
+        veg = findViewById(R.id.vegecard);
+
+        veg.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == veg) {
+            startActivity(new Intent(this, vegetables.class));
+        }
     }
 }
