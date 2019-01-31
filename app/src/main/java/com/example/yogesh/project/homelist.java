3b3102715766bb6c58ec7d059a9d016511bbf008
viola-grace <violagrace22@gmail.com>
2019-01-31 08:42:35 +0530

    homelist

--- app/src/main/java/com/example/yogesh/project/homelist.java
+++ app/src/main/java/com/example/yogesh/project/homelist.java
@@ -1,7 +1,10 @@
 package com.example.yogesh.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class homelist extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class homelist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homelist);
+
     }
 }
