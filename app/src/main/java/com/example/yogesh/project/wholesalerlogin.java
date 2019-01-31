b8f9fa90ad59d370965acb4487c7c71c7ecd1f2d
viola-grace <violagrace22@gmail.com>
2019-01-31 20:30:11 +0530

    homelist

--- app/src/main/java/com/example/yogesh/project/wholesalerlogin.java
+++ app/src/main/java/com/example/yogesh/project/wholesalerlogin.java
@@ -1,13 +1,36 @@
 package com.example.yogesh.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class wholesalerlogin extends AppCompatActivity {
 
+    private Button signupbtn;
+    private Button loginsuccess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wholesalerlogin);
+
+
+        signupbtn=(Button)findViewById(R.id.signup);
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(wholesalerlogin.this,wholesaler_signup.class);
+                startActivity(i);
+            }
+        });
+        loginsuccess=(Button)findViewById(R.id.btn_login);
+        loginsuccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(wholesalerlogin.this,wholesaler_signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
